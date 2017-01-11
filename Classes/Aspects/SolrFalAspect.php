c2a47931e8168c6c5a68ab8eddf6cfb7dd4147b8
Frans Saris <frans@beech.it>
2017-01-11 11:01:12 +0100

    [TASK] Update to newer TYPO3 CGL

--- Classes/Aspects/SolrFalAspect.php
+++ Classes/Aspects/SolrFalAspect.php
@@ -7,248 +7,254 @@ namespace BeechIt\SolrfalTextextract\Aspects;
  * All code (c) Beech Applications B.V. all rights reserved
  */
 
-use TYPO3\CMS\Core\SingletonInterface;
 use TYPO3\CMS\Core\Resource\File;
+use TYPO3\CMS\Core\SingletonInterface;
 use TYPO3\CMS\Core\Utility\CommandUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use BeechIt\FalSecuredownload\Security\CheckPermissions;
 use TYPO3\CMS\Core\Utility\PathUtility;
-use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 use TYPO3\Solr\Solrfal\Queue\Item;
 
 /**
  * Class SolrFalAspect
  */
-class SolrFalAspect implements SingletonInterface {
+class SolrFalAspect implements SingletonInterface
+{
 
-	/**
-	 * @var string
-	 */
-	protected $pathTika = '/usr/bin';
+    /**
+     * @var string
+     */
+    protected $pathTika = '/usr/bin';
 
-	/**
-	 * @var string
-	 */
-	protected $pathPdftotext;
+    /**
+     * @var string
+     */
+    protected $pathPdftotext;
 
-	/**
-	 * @var array
-	 */
-	protected $supportedFileExtensions = array();
+    /**
+     * @var array
+     */
+    protected $supportedFileExtensions = array();
 
-	/**
-	 * @var bool
-	 */
-	protected $debug = TRUE;
+    /**
+     * @var bool
+     */
+    protected $debug = TRUE;
 
-	/**
-	 * Contructor
-	 */
-	public function __construct() {
-		$extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['solrfal_textextract']);
-		if (!empty($extConf['pathTika'])) {
-			$this->pathTika = $extConf['pathTika'];
-			if (!GeneralUtility::isAbsPath($this->pathTika)) {
-				$this->pathTika = PathUtility::getCanonicalPath(PATH_site . $this->pathTika);
-			}
-			$this->pathTika = GeneralUtility::getFileAbsFileName($this->pathTika, FALSE);
-			if (!@is_file($this->pathTika)) {
-				$this->pathTika = NULL;
-			}
-		}
-		if (!empty($extConf['pathPdftotext'])) {
-			$this->pathPdftotext = $extConf['pathPdftotext'];
-		}
-		if (!empty($extConf['supportedFileExtensions'])) {
-			$this->supportedFileExtensions = GeneralUtility::trimExplode(',', $extConf['supportedFileExtensions']);
-		}
-		$this->debug = !empty($extConf['debugMode']);
+    /**
+     * Contructor
+     */
+    public function __construct()
+    {
+        $extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['solrfal_textextract']);
+        if (!empty($extConf['pathTika'])) {
+            $this->pathTika = $extConf['pathTika'];
+            if (!GeneralUtility::isAbsPath($this->pathTika)) {
+                $this->pathTika = PathUtility::getCanonicalPath(PATH_site . $this->pathTika);
+            }
+            $this->pathTika = GeneralUtility::getFileAbsFileName($this->pathTika, FALSE);
+            if (!@is_file($this->pathTika)) {
+                $this->pathTika = NULL;
+            }
+        }
+        if (!empty($extConf['pathPdftotext'])) {
+            $this->pathPdftotext = $extConf['pathPdftotext'];
+        }
+        if (!empty($extConf['supportedFileExtensions'])) {
+            $this->supportedFileExtensions = GeneralUtility::trimExplode(',', $extConf['supportedFileExtensions']);
+        }
+        $this->debug = !empty($extConf['debugMode']);
 
-		if ($this->debug) {
-			$messages = array();
-			if (!$this->pathTika) {
-				$messages[] = 'Tika jar not found.';
-			}
-			if (!$this->pathPdftotext) {
-				$messages[] = 'No pdftotext path set';
-			}
-			if ($this->supportedFileExtensions === array()) {
-				$messages[] = 'No supported file extensions set';
-			}
-			if ($messages !== array()) {
-				$this->writeLog('Configuration error: ' . implode(',', $messages), 3);
-			}
-		}
-	}
+        if ($this->debug) {
+            $messages = array();
+            if (!$this->pathTika) {
+                $messages[] = 'Tika jar not found.';
+            }
+            if (!$this->pathPdftotext) {
+                $messages[] = 'No pdftotext path set';
+            }
+            if ($this->supportedFileExtensions === array()) {
+                $messages[] = 'No supported file extensions set';
+            }
+            if ($messages !== array()) {
+                $this->writeLog('Configuration error: ' . implode(',', $messages), 3);
+            }
+        }
+    }
 
-	/**
-	 * Add correct fe_group info and public_url
-	 *
-	 * @param Item $item
-	 * @param \ArrayObject $metadata
-	 */
-	public function fileMetaDataRetrieved(Item $item, \ArrayObject $metadata) {
+    /**
+     * @param $message
+     * @param int $level
+     */
+    protected function writeLog($message, $level = 0)
+    {
+        /** @var \TYPO3\CMS\Core\Authentication\BackendUserAuthentication $backendUser */
+        $backendUser = $GLOBALS['BE_USER'];
+        if (!empty($backendUser)) {
+            $backendUser->writelog(4, 1, $level, 0, '[solrfal_textextract] ' . $message, array());
+        }
+    }
 
-		if ($item->getFile() instanceof File && in_array(mb_strtolower($item->getFile()->getExtension()), $this->supportedFileExtensions)) {
-			$content = NULL;
-			if ($item->getFile()->getExtension() === 'pdf') {
-				$content = $this->pdfToText($item->getFile());
-			}
-			if ($content !== NULL && $this->pathTika) {
-				$content = $this->fileToText($item->getFile());
-			}
-			if ($content !== NULL) {
-				$metadata['content'] = $content;
-			}
-		}
-	}
+    /**
+     * Add correct fe_group info and public_url
+     *
+     * @param Item $item
+     * @param \ArrayObject $metadata
+     */
+    public function fileMetaDataRetrieved(Item $item, \ArrayObject $metadata)
+    {
 
-	/**
-	 * Use pdftotext to extract contents of pdf file
-	 *
-	 * @param File $file
-	 * @return string
-	 */
-	protected function pdfToText(File $file) {
-		if ($this->isPdfEncrypted($file)) {
-			return '';
-		}
-		$tempFile = GeneralUtility::tempnam('pdfToText');
-		$cmd = rtrim($this->pathPdftotext, '/') . '/pdftotext -enc UTF-8 -q '
-			  . escapeshellarg($file->getForLocalProcessing(FALSE))
-			  . ' ' . $tempFile;
-		exec($cmd);
-		$content = file_get_contents($tempFile);
-		GeneralUtility::unlink_tempfile($tempFile);
+        if ($item->getFile() instanceof File && in_array(mb_strtolower($item->getFile()->getExtension()), $this->supportedFileExtensions)) {
+            $content = NULL;
+            if ($item->getFile()->getExtension() === 'pdf') {
+                $content = $this->pdfToText($item->getFile());
+            }
+            if ($content !== NULL && $this->pathTika) {
+                $content = $this->fileToText($item->getFile());
+            }
+            if ($content !== NULL) {
+                $metadata['content'] = $content;
+            }
+        }
+    }
 
-		// Last check for encrypted document
-		if ($this->textHasEncryptionMarks($content)) {
-			$content = NULL;
-		}
+    /**
+     * Use pdftotext to extract contents of pdf file
+     *
+     * @param File $file
+     * @return string
+     */
+    protected function pdfToText(File $file)
+    {
+        if ($this->isPdfEncrypted($file)) {
+            return '';
+        }
+        $tempFile = GeneralUtility::tempnam('pdfToText');
+        $cmd = rtrim($this->pathPdftotext, '/') . '/pdftotext -enc UTF-8 -q '
+            . escapeshellarg($file->getForLocalProcessing(FALSE))
+            . ' ' . $tempFile;
+        exec($cmd);
+        $content = file_get_contents($tempFile);
+        GeneralUtility::unlink_tempfile($tempFile);
 
-		return $content;
-	}
+        // Last check for encrypted document
+        if ($this->textHasEncryptionMarks($content)) {
+            $content = NULL;
+        }
 
-	/**
-	 * Check if pdf is encrypted
-	 *
-	 * @param File $file
-	 * @return bool
-	 */
-	protected function isPdfEncrypted(File $file) {
-		$encrypted = FALSE;
-		$cmd = rtrim($this->pathPdftotext, '/') . '/pdfinfo '
-			. escapeshellarg($file->getForLocalProcessing(FALSE));
-		CommandUtility::exec($cmd, $pdfInfoArray);
+        return $content;
+    }
 
-		$form = '';
-		$version = 0;
-		$copyEncrypted = FALSE;
-		$changeEncrypted = FALSE;
-		$optimized = FALSE;
-		$pageFormatA4 = FALSE;
+    /**
+     * Check if pdf is encrypted
+     *
+     * @param File $file
+     * @return bool
+     */
+    protected function isPdfEncrypted(File $file)
+    {
+        $encrypted = FALSE;
+        $cmd = rtrim($this->pathPdftotext, '/') . '/pdfinfo '
+            . escapeshellarg($file->getForLocalProcessing(FALSE));
+        CommandUtility::exec($cmd, $pdfInfoArray);
 
-		// Find some info about pdf to determine if we can read its contents
-		foreach ($pdfInfoArray as $line) {
-			list($key, $value) = explode(':', $line, 2);
-			$value = trim($value);
+        $form = '';
+        $version = 0;
+        $copyEncrypted = FALSE;
+        $changeEncrypted = FALSE;
+        $optimized = FALSE;
+        $pageFormatA4 = FALSE;
 
-			if ($key === 'Encrypted') {
-				if ($value !== 'no') {
-					$encrypted = TRUE;
-					$copyEncrypted = strpos($value, 'copy:no') === FALSE;
-					$changeEncrypted = strpos($value, 'change:no') === FALSE;
-				}
-			}
+        // Find some info about pdf to determine if we can read its contents
+        foreach ($pdfInfoArray as $line) {
+            list($key, $value) = explode(':', $line, 2);
+            $value = trim($value);
 
-			if ($key === 'Form') {
-				$form = $value;
-			}
-			if ($key === 'PDF version') {
-				$version = (float)$value;
-			}
-			if ($key === 'Optimized') {
-				$optimized = ($value === 'Yes');
-			}
-			if ($key === 'Page size') {
-				$pageFormatA4 = (strpos($value, 'A4') === FALSE);
-			}
-		}
+            if ($key === 'Encrypted') {
+                if ($value !== 'no') {
+                    $encrypted = TRUE;
+                    $copyEncrypted = strpos($value, 'copy:no') === FALSE;
+                    $changeEncrypted = strpos($value, 'change:no') === FALSE;
+                }
+            }
 
-		// Forms are readable (AcroForm we know for sure, but we expect all forms)
-		if ($form !== 'none') {
-			$encrypted = FALSE;
-		}
-		// Version < 1.6 can also be read if copy of change isn't encrypted
-		if ($version < 1.6 && (!$copyEncrypted || !$changeEncrypted)) {
-			$encrypted = FALSE;
-		}
+            if ($key === 'Form') {
+                $form = $value;
+            }
+            if ($key === 'PDF version') {
+                $version = (float)$value;
+            }
+            if ($key === 'Optimized') {
+                $optimized = ($value === 'Yes');
+            }
+            if ($key === 'Page size') {
+                $pageFormatA4 = (strpos($value, 'A4') === FALSE);
+            }
+        }
 
-		// PDF version 1.6 is also readable for if not optimized
-		if ($version === 1.6 && !$optimized) {
-			$encrypted = FALSE;
-		}
-		// PDF version 1.6 and no A4 value is also (most times) readable
-		if ($version === 1.6 && !$pageFormatA4) {
-			$encrypted = FALSE;
-		}
+        // Forms are readable (AcroForm we know for sure, but we expect all forms)
+        if ($form !== 'none') {
+            $encrypted = FALSE;
+        }
+        // Version < 1.6 can also be read if copy of change isn't encrypted
+        if ($version < 1.6 && (!$copyEncrypted || !$changeEncrypted)) {
+            $encrypted = FALSE;
+        }
 
-		return $encrypted;
-	}
+        // PDF version 1.6 is also readable for if not optimized
+        if ($version === 1.6 && !$optimized) {
+            $encrypted = FALSE;
+        }
+        // PDF version 1.6 and no A4 value is also (most times) readable
+        if ($version === 1.6 && !$pageFormatA4) {
+            $encrypted = FALSE;
+        }
 
-	/**
-	 * Check if text has markers that are consistent with an encrypted document
-	 *
-	 * @param $text
-	 * @return bool
-	 */
-	protected function textHasEncryptionMarks($text) {
-		if (strpos($text, '%#$#') !== FALSE) {
-			return TRUE;
-		}
-		if (strpos($text, '!%!') !== FALSE) {
-			return TRUE;
-		}
-		return FALSE;
-	}
+        return $encrypted;
+    }
 
-	/**
-	 * Use tika to extract contents of pdf file
-	 *
-	 * @param File $file
-	 * @return string
-	 */
-	protected function fileToText(File $file) {
-		$content = NULL;
-		$tikaCommand = CommandUtility::getCommand('java')
-			. ' -Dfile.encoding=UTF8' // forces UTF8 output
-			. ' -jar ' . escapeshellarg($this->pathTika)
-			. ' -t'
-			. ' ' . escapeshellarg($file->getForLocalProcessing(FALSE));
+    /**
+     * Check if text has markers that are consistent with an encrypted document
+     *
+     * @param $text
+     * @return bool
+     */
+    protected function textHasEncryptionMarks($text)
+    {
+        if (strpos($text, '%#$#') !== FALSE) {
+            return TRUE;
+        }
+        if (strpos($text, '!%!') !== FALSE) {
+            return TRUE;
+        }
+        return FALSE;
+    }
 
-		exec($tikaCommand, $output);
+    /**
+     * Use tika to extract contents of pdf file
+     *
+     * @param File $file
+     * @return string
+     */
+    protected function fileToText(File $file)
+    {
+        $content = NULL;
+        $tikaCommand = CommandUtility::getCommand('java')
+            . ' -Dfile.encoding=UTF8' // forces UTF8 output
+            . ' -jar ' . escapeshellarg($this->pathTika)
+            . ' -t'
+            . ' ' . escapeshellarg($file->getForLocalProcessing(FALSE));
 
-		if ($output) {
-			$content = implode(PHP_EOL, $output);
+        exec($tikaCommand, $output);
 
-			// Last check for encrypted document
-			if ($this->textHasEncryptionMarks($content)) {
-				$content = NULL;
-			}
-		}
+        if ($output) {
+            $content = implode(PHP_EOL, $output);
 
-		return $content;
-	}
+            // Last check for encrypted document
+            if ($this->textHasEncryptionMarks($content)) {
+                $content = NULL;
+            }
+        }
 
-	/**
-	 * @param $message
-	 * @param int $level
-	 */
-	protected function writeLog($message, $level = 0) {
-		/** @var \TYPO3\CMS\Core\Authentication\BackendUserAuthentication $backendUser */
-		$backendUser = $GLOBALS['BE_USER'];
-		if (isset($backendUser)) {
-			$backendUser->writelog(4, 1, $level, 0, '[solrfal_textextract] ' . $message, array());
-		}
-	}
+        return $content;
+    }
 }
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,18 +1,19 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
-}
+
+defined('TYPO3_MODE') || die('Access denied.');
 
 if (TYPO3_MODE === 'BE') {
 
-	/** @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher $signalSlotDispatcher */
-	$signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher');
+    /** @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher $signalSlotDispatcher */
+    $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+        'TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher'
+    );
 
-	// ext:solrfal enrich metadata and generate correct public url slot
-	$signalSlotDispatcher->connect(
-		'TYPO3\\Solr\\Solrfal\\Indexing\\DocumentFactory',
-		'fileMetaDataRetrieved',
-		'BeechIt\\SolrfalTextextract\\Aspects\\SolrFalAspect',
-		'fileMetaDataRetrieved'
-	);
+    // ext:solrfal enrich metadata and generate correct public url slot
+    $signalSlotDispatcher->connect(
+        'TYPO3\\Solr\\Solrfal\\Indexing\\DocumentFactory',
+        'fileMetaDataRetrieved',
+        'BeechIt\\SolrfalTextextract\\Aspects\\SolrFalAspect',
+        'fileMetaDataRetrieved'
+    );
 }
